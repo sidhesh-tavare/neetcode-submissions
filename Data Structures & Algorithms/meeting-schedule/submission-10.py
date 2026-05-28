8dbf8ddff96ee1d55264a7dc6b2d5cd1e0183111
Sidhesh Tavare <153364937+sidhesh-tavare@users.noreply.github.com>
2026-05-28 13:46:05 +0530

    Add: meeting-schedule - submission-10

---
+++ Data Structures & Algorithms/meeting-schedule/submission-10.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals: return True 
+        intervals = [[inte.start,inte.end] for inte in intervals]
+        intervals.sort()
+        n = len(intervals)
+        pend = intervals[0][1]
+        for i in range(1,n):
+            cs,ce = intervals[i]
+            if cs<pend:
+                return False
+            pend = max(ce,pend)
+        return True
