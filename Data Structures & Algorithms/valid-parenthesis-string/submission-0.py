db36172fe6856238a0d67dd27e26c6088f66bbd4
Sidhesh Tavare <153364937+sidhesh-tavare@users.noreply.github.com>
2026-06-01 17:19:25 +0530

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left,star = [],[]
+        for i,char in enumerate(s):
+            if char =="(": left.append(i)
+            elif char=="*":star.append(i)
+            else:
+                if left: left.pop()
+                elif star: star.pop()
+                else: return False
+        
+        while star and left:
+            star_idx = star.pop()
+            left_idx = left.pop()
+            if star_idx<left_idx:
+                return False
+        
+        if left: return False
+        return True
+
+
+        
